60489d71797a5c6f8841aea7205e368c14810ac5
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2023-02-24 09:03:37 -0800

    Create open_urls_from_file_find_all_links.py

---
+++ open_urls_from_file_find_all_links.py
@@ -0,0 +1,18 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+with open('urls.txt') as f:
+  urls = [ line.rstrip() for line in f]
+  
+links = []
+for url in urls:
+    page = urlopen(url)
+    print(f"Responce Code for {url}: {page.getcode}\n")
+  
+    soup = BeautifulSoup(page, "html.parser")
+    for link in soup.find_all(attrs={'href': re.compile("http")}):
+        links.append(link.get('href')
+          
+for link in links:
+    print(link)
