80be39ce391fa191c4fc532e5053111a7fc754dc
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2023-02-24 11:40:05 -0800

    Update scrape_cba_am.py

--- scrape_cba_am.py
+++ scrape_cba_am.py
@@ -1,29 +1,46 @@
 # this script is not ready yet
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # 
+import ssl
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import tld
 import re
 
+# this line fixes some errors that I did have time to research
+# taken form stackoverflow, seems to work
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# the only domain we want to scrape
 domain_scraped = "cba.am"
-with open('urls.txt') as f:
-  urls = [ line.rstrip() for line in f]
 
-visited_urls = {}
 
+#with open('urls.txt') as f:
+#  urls = [ line.rstrip() for line in f]
+
+urls = ["https://cba.am"]
+visited_urls = set() 
+SCRAPE_COUNT = 10
 pages_scraped = 0
 
 for url in urls:
+    print(f"Current URl being scraped: {url}\n")
     # add the url to the visisted url set
-    visited_urls += url
+    visited_urls.add(url)
     
     # create a list, which will hold all links in the website
     links = []
     
-    #open the website and get the responce code
-    page = urlopen(url)
-    print(f"Responce Code for {url}: {page.getcode}\n")
+    try:
+        #open the website and get the responce code
+        page = urlopen(url)
+    except:
+        print("!!!!!!!!!!!!!!!!!!!!!!!\n")
+        print(f"Could not open url: {url}")
+        print("!!!!!!!!!!!!!!!!!!!!!!!\n\n")
+        pages_scraped += 1
+        continue
+    print(f"Responce Code for {url}: {page.getcode()}\n\n")
     
     # find all links in the current url, and add it to the links list
     soup = BeautifulSoup(page, "html.parser")
@@ -37,13 +54,15 @@ for url in urls:
         res = tld.get_tld(link, as_object=True)
         dom = f"{res.domain}.{res}"
         #if the domain.tld is not cba.am continue
-        if dom != "cba.am":
+        if dom != domain_scraped:
+            print(dom)
             continue
         else:
             if(link not in visited_urls):
-                     url += link
+                print(f"appending link: {link}")
+                urls.append(link)
     pages_scraped += 1
-    if(pages_scraped > 1000):
+    if(pages_scraped > SCRAPE_COUNT):
         break
                      
-print("scraping finished, scanned 1000 websites!")
+print("scraping finished, scanned {SCRAPE_COUNT} websites!")
