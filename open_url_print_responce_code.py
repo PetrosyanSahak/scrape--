b23402f8721314c4329db36d14b557c265f6b360
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2023-02-24 08:41:02 -0800

    Create open_url_print_responce_code.py

---
+++ open_url_print_responce_code.py
@@ -0,0 +1,11 @@
+# open youtube.com and print the responce code
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = "https://youtube.com"
+
+website = urlopen(url)
+print(website.getcode())
+
+
