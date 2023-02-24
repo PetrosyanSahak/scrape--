aa29244401e2278ddd2083e6e311b66e0ea5e848
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2023-02-24 09:46:04 -0800

    Create get_domain_and_tld.py

---
+++ get_domain_and_tld.py
@@ -0,0 +1,14 @@
+# this script gets the 'main' domain with the tld
+# we have certain domains that we should not scan
+# so we will check if the url belongs to the restricted domains
+
+from urllib.parse import urlparse
+import tld
+
+url = "https://www.youtube.com/watch?v=cE72CpmIcFk"
+res = tld.get_tld(url, as_object=True)
+print(f"{res.domain}.{res}") # should print "youtube.com"
+
+url = "https://www.cba.am/am/SitePages/mpobjective.aspx"
+res = tld.get_tld(url, as_object=True)
+print(f"{res.domain}.{res}") # should print "cba.am"
