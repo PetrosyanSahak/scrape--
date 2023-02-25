5a3623ec0ea58e0645a12a07d128471ae36cbe59
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2023-02-24 21:05:57 -0800

    Update check_rss_availability.py

--- check_rss_availability.py
+++ check_rss_availability.py
@@ -1,2 +1,18 @@
-# should get all links, and check if there is a link
-# which is of type application/rss+xml
+# check if there is an RSS feed
+# check all links in the html, if there is a link
+# of type application/rss+xml then that link has RSS feed
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+url = "https://www.schneier.com/"
+
+page = urlopen(url)
+# print(page.getcode())
+
+soup = BeautifulSoup(page, "html.parser")
+
+for link in soup.find_all(attrs={'href': re.compile("http")}):
+  if link.get('type') == "application/rss+xml":
+      print(f"RSS feed available for {link}")
+  
