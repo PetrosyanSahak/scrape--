2d3ffcf6d992011310ef303eb7caff0a49cea816
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2023-02-27 10:36:00 -0800

    Update url_class.py

--- url_class.py
+++ url_class.py
@@ -5,12 +5,12 @@ class url:
       # add scheme and domain if url is relative (https:// + example.com)
       # url unquote if applicable
     def __init__(self, url = '', parent_urls = [], is_relative = False, parent_scheme_domain = '', response_code = 0, rss_avail = False):
-      url_m = ''
-      parent_urls_m = []
-      is_relative_m = False
-      parent_scheme_domain_m = '' # empty if the path is not relative
-      response_code_m = 0
-      rss_avail_m = False
+      url_m = url
+      parent_urls_m = parent_urls
+      is_relative_m = is_relative
+      parent_scheme_domain_m = parent_scheme_domain # empty if the path is not relative
+      response_code_m = response_code
+      rss_avail_m = rss_avail
       
  
     
