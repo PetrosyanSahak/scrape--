5eaffd8a2b602b7ca67acaeef4d4220b3bcfe9d2
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2023-02-27 12:40:14 -0800

    Update url_class.py

--- url_class.py
+++ url_class.py
@@ -28,7 +28,7 @@ class Url:
       # construct the full url, so that we can open it
       # add scheme and domain if url is relative (https:// + example.com)
       # url unquote if applicable
-    def __init__(self, url = '', parent_urls = [], parent_scheme_domain = '', response_code = 0, rss_avail = False):
+    def __init__(self, url, parent_urls = [], parent_scheme_domain = '', response_code = 0, rss_avail = False):
       self.url_m = url
       self.parent_urls_m = parent_urls
       self.parent_scheme_domain_m = parent_scheme_domain # empty if the path is not relative
@@ -42,7 +42,7 @@ class Url:
     
 url = "https%3A%2F%2F%D5%A1%D5%B6%D5%A4%D6%80%D5%A1%D5%B6%D5%AB%D5%AF.%D5%B0%D5%A1%D5%B5%2F/գրառում/2023/01/"
 #https://անդրանիկ.հայ/գրառում/2023/01/
-newurl  = Url(url = url)
+newurl  = Url(url)
 print(newurl.get_full_url())
 website = urlopen(newurl.get_full_url())
 soup = BeautifulSoup(website, "html.parser")
