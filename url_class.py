d5cea850217a7b63b6f991c7645b39d455025318
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2023-02-27 10:32:06 -0800

    Create url_class.py

---
+++ url_class.py
@@ -0,0 +1,15 @@
+class url:
+    
+    def get_full_url(self):
+      # construct the full url, so that we can open it
+      # url unquote if applicable
+    def __init__(self, url = '', parent_urls = [], is_relative = False, parent_scheme_domain = '', response_code = 0, rss_avail = False):
+      url_m = ''
+      parent_urls_m = []
+      is_relative_m = False
+      parent_scheme_domain_m = '' # empty if the path is not relative
+      response_code_m = 0
+      rss_avail_m = False
+      
+ 
+    
