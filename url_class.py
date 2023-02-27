2951e10e62f66fbed21dcea07573a988a97016ed
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2023-02-27 11:34:54 -0800

    Update url_class.py

--- url_class.py
+++ url_class.py
@@ -7,16 +7,16 @@ from urllib.parse import quote
 from urllib.parse import unquote
 from urllib.request import urlopen
 
-url = "https%3A%2F%2F%D5%A1%D5%B6%D5%A4%D6%80%D5%A1%D5%B6%D5%AB%D5%AF.%D5%B0%D5%A1%D5%B5%2F"
-url1 = unquote(url)
-d = urllib.parse.urlparse(url1)
-result = d.scheme + "://" + idna.encode(d.netloc).decode('ascii')
+#url = "https%3A%2F%2F%D5%A1%D5%B6%D5%A4%D6%80%D5%A1%D5%B6%D5%AB%D5%AF.%D5%B0%D5%A1%D5%B5%2F"
+#url1 = unquote(url)
+#d = urllib.parse.urlparse(url1)
+#result = d.scheme + "://" + idna.encode(d.netloc).decode('ascii')
 #urlopen(url)
-print(result)
+#print(result)
 #website  = requests.get(result)
-website  = urlopen(result)
-soup = BeautifulSoup(website, 'html.parser')
-print(soup)
+#website  = urlopen(result)
+#soup = BeautifulSoup(website, 'html.parser')
+#print(soup)
 
 
 class url:
@@ -38,7 +38,6 @@ class url:
     def __init__(self, url = '', parent_urls = [], parent_scheme_domain = '', response_code = 0, rss_avail = False):
       url_m = url
       parent_urls_m = parent_urls
-      
       parent_scheme_domain_m = parent_scheme_domain # empty if the path is not relative
       response_code_m = response_code
       rss_avail_m = rss_avail
